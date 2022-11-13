e1eeac61572ef7dc21de8f7cf38a745758f73aa5
ViraKryvets <vkrivets@gmail.com>
2022-11-13 12:23:20 +0200

    add file lesson.rb

---
+++ lesson.rb
@@ -0,0 +1,12 @@
+# puts('What's your name? ')
+# name = gets.chomp()
+# puts('Hello, ' + name)
+
+# puts('Enter num1')
+# first_number = gets.chomp().to_i
+# puts('Enter num2')
+# second_number = gets.chomp().to_i
+# result = (first_number*second_number).to_s
+# puts('Result is ' + result)
+
+
